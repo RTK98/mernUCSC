62665960a574a4c351727f7671ede312262d83e5
Rajindra Tharindu <rajindratharindu@gmail.com>
2023-12-29 09:32:30 +0530

    Add NavBar Component

---
+++ src/components/navBar/navBar.jsx
@@ -0,0 +1,14 @@
+
+function NavBar() {
+  return (
+    <div className="container">
+        <ul className="navBarUl">
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+        </ul>
+    </div>
+  );
+}
+export default NavBar;
